b1dfd80388ff67a6f7fead3eac442c56230ef7cf
r00t_hub <105140943+woojin03@users.noreply.github.com>
2025-03-27 21:11:52 +0900

    Create dynamic_report.js

---
+++ output/dynamic_report.js
@@ -0,0 +1,180 @@
+const dynamicReport = {
+  "hash": "f90f81f7b47ca73de0e5aa5aaeba6735",
+  "emails": [
+    "framework@boot-conscrypt.art",
+    "framework@boot-bouncycastle.art",
+    "framework@boot-okhttp.art",
+    "framework@boot-ext.art",
+    "framework@boot.art",
+    "framework@boot-voip-common.art",
+    "framework@boot-apache-xml.art",
+    "202414022@20.24",
+    "framework@boot-core-libart.art",
+    "framework@boot-android.test",
+    "framework@boot-android.hidl",
+    "framework@boot-ims-common.art",
+    "framework@boot-framework.art"
+  ],
+  "urls": [
+    "https://dl.google.com/handwriting/models/handwriting.superpack_manifest.20180709.json",
+    "https://www.googleapis.com/auth/contextcontroller",
+    "https://www.gstatic.com/android/keyboard/stickers/configs/v5/,",
+    "https://www.gstatic.com/android/keyboard/dictionarypack/ney-normal/metadata.json",
+    "https://android.googleapis.com/checkin",
+    "https://dl.google.com/handwriting/models/handwriting.superpack_manifest.20180709.json'",
+    "www.google.com",
+    "https://www.google.com/generate_204",
+    "http://connectivitycheck.gstatic.com/generate_204",
+    "http://goo.gl/8rd3yj",
+    "https://www.gstatic.com/android/keyboard/modelpack/emoji/20180831175952/superpacks_manifest.zip"
+  ],
+  "domains": {
+    "www.gstatic.com": {
+      "bad": "no",
+      "geolocation": {
+        "ip": "142.250.76.131",
+        "country_short": "US",
+        "country_long": "United States of America",
+        "region": "California",
+        "city": "Mountain View",
+        "latitude": "37.405991",
+        "longitude": "-122.078514"
+      },
+      "ofac": false
+    },
+    "connectivitycheck.gstatic.com": {
+      "bad": "no",
+      "geolocation": {
+        "ip": "142.250.207.99",
+        "country_short": "US",
+        "country_long": "United States of America",
+        "region": "California",
+        "city": "Mountain View",
+        "latitude": "37.405991",
+        "longitude": "-122.078514"
+      },
+      "ofac": false
+    },
+    "www.googleapis.com": {
+      "bad": "no",
+      "geolocation": {
+        "ip": "142.250.207.106",
+        "country_short": "US",
+        "country_long": "United States of America",
+        "region": "California",
+        "city": "Mountain View",
+        "latitude": "37.405991",
+        "longitude": "-122.078514"
+      },
+      "ofac": false
+    },
+    "android.googleapis.com": {
+      "bad": "no",
+      "geolocation": {
+        "ip": "142.250.206.234",
+        "country_short": "US",
+        "country_long": "United States of America",
+        "region": "California",
+        "city": "Mountain View",
+        "latitude": "37.405991",
+        "longitude": "-122.078514"
+      },
+      "ofac": false
+    },
+    "www.google.com": {
+      "bad": "no",
+      "geolocation": {
+        "ip": "172.217.25.164",
+        "country_short": "US",
+        "country_long": "United States of America",
+        "region": "California",
+        "city": "Mountain View",
+        "latitude": "37.405991",
+        "longitude": "-122.078514"
+      },
+      "ofac": false
+    },
+    "goo.gl": {
+      "bad": "no",
+      "geolocation": {
+        "ip": "142.250.206.206",
+        "country_short": "US",
+        "country_long": "United States of America",
+        "region": "California",
+        "city": "Mountain View",
+        "latitude": "37.405991",
+        "longitude": "-122.078514"
+      },
+      "ofac": false
+    },
+    "dl.google.com": {
+      "bad": "no",
+      "geolocation": {
+        "ip": "142.250.207.110",
+        "country_short": "US",
+        "country_long": "United States of America",
+        "region": "California",
+        "city": "Mountain View",
+        "latitude": "37.405991",
+        "longitude": "-122.078514"
+      },
+      "ofac": false
+    }
+  },
+  "clipboard": [],
+  "xml": [
+    {
+      "type": "xml",
+      "file": "C:/Users/oneth/.MobSF/uploads/f90f81f7b47ca73de0e5aa5aaeba6735/DYNAMIC_DeviceData\\datadatacom.ldjSxw.heBbQdapp_com.ldjSxw.heBbQd.MainApplication_nullappAndroidManifest.xml"
+    }
+  ],
+  "sqlite": [],
+  "others": [
+    {
+      "type": "others",
+      "file": "C:/Users/oneth/.MobSF/uploads/f90f81f7b47ca73de0e5aa5aaeba6735/DYNAMIC_DeviceData\\datadatacom.ldjSxw.heBbQdapp_com.ldjSxw.heBbQd.MainApplication_nullappassetspgsHZz.apk"
+    },
+    {
+      "type": "others",
+      "file": "C:/Users/oneth/.MobSF/uploads/f90f81f7b47ca73de0e5aa5aaeba6735/DYNAMIC_DeviceData\\datadatacom.ldjSxw.heBbQdfilesall_process_once_done"
+    },
+    {
+      "type": "others",
+      "file": "C:/Users/oneth/.MobSF/uploads/f90f81f7b47ca73de0e5aa5aaeba6735/DYNAMIC_DeviceData\\datadatacom.ldjSxw.heBbQdfilesappstate"
+    },
+    {
+      "type": "others",
+      "file": "C:/Users/oneth/.MobSF/uploads/f90f81f7b47ca73de0e5aa5aaeba6735/DYNAMIC_DeviceData\\datadatacom.ldjSxw.heBbQdfilesfirst_open_date"
+    },
+    {
+      "type": "others",
+      "file": "C:/Users/oneth/.MobSF/uploads/f90f81f7b47ca73de0e5aa5aaeba6735/DYNAMIC_DeviceData\\datadatacom.ldjSxw.heBbQdfileslast_scan_check_date"
+    },
+    {
+      "type": "others",
+      "file": "C:/Users/oneth/.MobSF/uploads/f90f81f7b47ca73de0e5aa5aaeba6735/DYNAMIC_DeviceData\\datadatacom.ldjSxw.heBbQdfilespgsHZz.apk"
+    }
+  ],
+  "tls_tests": {
+    "tls_misconfigured": false,
+    "no_tls_pin_or_transparency": false,
+    "pin_or_transparency_bypassed": false,
+    "has_cleartext": false
+  },
+  "screenshots": [],
+  "activity_tester": {},
+  "exported_activity_tester": {},
+  "droidmon": {},
+  "apimon": {},
+  "base64_strings": [],
+  "trackers": {
+    "detected_trackers": 0,
+    "total_trackers": 432,
+    "trackers": []
+  },
+  "frida_logs": true,
+  "runtime_dependencies": [],
+  "package": "com.ldjSxw.heBbQd",
+  "version": "v4.3.1",
+  "title": "Dynamic Analysis"
+};
